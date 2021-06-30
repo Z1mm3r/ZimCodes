daa983265485444ca49844a3e59ce717dd100c0e
Z1mm3r <czimmer@ramapo.edu>
2021-06-30 10:37:24 -0400

    styling

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -3,6 +3,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import cx from 'classnames'
+import Grid from '@material-ui/core/Grid'
 import {makeStyles} from '@material-ui/styles'
 import { useTheme } from '@material-ui/core/styles';
 
@@ -14,19 +15,21 @@ const Logo = (props) => {
     const {full, path} = {...props}
     const classes = useStyle()
     return(
-        <Card className={cx({
-            [`${classes.fullHeight}`] : !!full ,
-        }
-        )}>
-            <CardContent>
-                <div>
-                    <CardMedia
-                        component="img"
-                        image={process.env.PUBLIC_URL +  (path ? path : "/Images/WIP-LOGO.png")}
-                        title="Zim Codes"
-                    />
-                </div>
-             </CardContent>
+        <Card className={cx({[`${classes.fullHeight}`] : !!full})}>
+            <Grid  align= "center" container direction="column" justify = "center" spacing={0} className={cx({[`${classes.fullHeight}`] : !!full})}>
+                <Grid items>
+                    <CardContent>
+                        <div>
+                            <CardMedia
+                                component="img"
+                                image={process.env.PUBLIC_URL +  (path ? path : "/Images/WIP-LOGO.png")}
+                                title="Zim Codes"
+                            />
+                        </div>
+                    </CardContent>
+                </Grid>
+    
+             </Grid>
         </Card>
     )
 }
