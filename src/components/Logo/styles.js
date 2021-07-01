d470254679565187662823a14e67a9d85ad88b0c
Z1mm3r <czimmer@ramapo.edu>
2021-07-01 14:35:57 -0400

    test

--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -2,8 +2,9 @@
 const styles ={
    fullHeight:{
        height:"100% !important"
-    }
+    },
  }
+
  
  
  export default styles
--- src/screens/IndexPage/index.js
+++ src/screens/IndexPage/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 
+import { animated, config,useSpring,useTransition } from '@react-spring/web'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+
+
 import AbsoluteWrapper from 'components/AbsoluteWrapper'
 import IndexHero from './IndexHero'
 import TechSection from './TechSection'
+
+
+/* TODO ADD PARALAX -> 
+      Fade in with  "scroll down for more" 
+                              ||
+                             \||/
+                              \/
+      Scroll down fades out when going down
+      Big logos come in from sides
+      Small logos come in from below
+*/
+
 
 const IndexPage = (props) => {
     const{
@@ -14,10 +30,12 @@ const IndexPage = (props) => {
         <AbsoluteWrapper>
             <Grid  container>
                 <Grid item xs={12}>
-                    Index Page
+                    <IndexHero/>
                 </Grid>
                 <Grid item xs={12}>
-                    <IndexHero/>
+                    <div>
+                        Scroll Down
+                    </div>
                 </Grid>
                 <Grid item xs={12}>
                     <TechSection/>
