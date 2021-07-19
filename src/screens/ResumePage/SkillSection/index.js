24f02f0a9cd96fda2192260eb38575dd93f96a7d
Z1mm3r <czimmer@ramapo.edu>
2021-07-19 16:14:10 -0400

    updated skills section, added personal information area

--- src/screens/ResumePage/SkillSection/index.js
+++ src/screens/ResumePage/SkillSection/index.js
@@ -2,35 +2,58 @@ import React from 'react'
 
 import SkillBar from './SkillBar'
 import Divider from '@material-ui/core/Divider'
+import Typography from '@material-ui/core/Typography'
 
 const Index = () =>{
     return(
         <>
-            Technical Skills
+            <Typography variant={"h5"}>
+                Technical Skills
+            </Typography>
             <Divider/>
-            React.js
+            <Typography align={"left"} variant={"h6"}>
+               React.js
+            </Typography>
             <SkillBar percentage={98}/>
-            Javascript
-            <SkillBar percentage={94}/>
-            Redux
+            <Typography align={"left"} variant={"h6"}>
+               Javascript
+            </Typography>
+            <SkillBar percentage={98}/>
+            <Typography align={"left"} variant={"h6"}>
+               Redux
+            </Typography>
             <SkillBar percentage={90}/>
-            Ruby
+            <Typography align={"left"} variant={"h6"}>
+               Ruby
+            </Typography>
             <SkillBar percentage={85}/>
-            HTML
+            <Typography align={"left"} variant={"h6"}>
+               HTML
+            </Typography>
             <SkillBar percentage={80}/>
-            CSS
+            <Typography align={"left"} variant={"h6"}>
+               CSS
+            </Typography>
             <SkillBar percentage={80}/>
 
             <Divider/>
             <br/>
-            Soft Skills
+            <Typography variant={"h5"}>
+                Soft Skills
+            </Typography>
             <Divider/>
-            Collaboration
+            <Typography align={"left"} variant={"h6"}>
+               Collaboration
+            </Typography>
             <SkillBar percentage={99}/>
-            Problem Solving
-            <SkillBar percentage={93}/>
-            Adaptability
-            <SkillBar percentage={90}/>
+            <Typography align={"left"} variant={"h6"}>
+               Problem Solving
+            </Typography>
+            <SkillBar percentage={98}/>
+            <Typography align={"left"} variant={"h6"}>
+               Adaptability
+            </Typography>
+            <SkillBar percentage={95}/>
 
         </>
     )
--- src/screens/ResumePage/index.js
+++ src/screens/ResumePage/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import AbsoluteWrapper from 'components/AbsoluteWrapper'
+import PersonalSection from './PersonalSection'
 import SkillSection from './SkillSection'
 
 import Divider from '@material-ui/core/Divider'
@@ -8,18 +9,28 @@ import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 
 const Index = () =>{
+
+    /*
+        TODO: 
+            Make Skillbar Lines thicker
+            Stylize SKills Section, Backgrounds etc
+
+
+    */
         return(
             <AbsoluteWrapper>
                 <Paper>
                     <Grid  container>
-                        <Grid item xs={5}>
+                        <Grid item xs={3}>
+                            <PersonalSection/>
+                            <Divider/>
                             <SkillSection/>        
                         </Grid>
                         <Grid item align="center" xs={1}>
                             <Divider orientation="vertical" />
                         </Grid>
 
-                        <Grid item xs={5}>
+                        <Grid item xs={7}>
                             scammin
                         </Grid>
                     </Grid>
